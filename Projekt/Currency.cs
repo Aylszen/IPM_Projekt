cad7f9f9e02b6d43fa592a9726bf038f4df7a108
Krzysiek Kita <aylszen@gmail.com>
2020-01-27 18:43:58 +0100

    CurrencyViewModel class created

--- Projekt/Currency.cs
+++ Projekt/Currency.cs
@@ -28,28 +28,6 @@ namespace CurrencyNamespace
             this.Price = Price;
         }
     }
-    public class CurrencyViewModel{
-
-        private ObservableCollection<Currency> currencies = new ObservableCollection<Currency>();
-        public ObservableCollection<Currency> Currencies { get { return this.currencies; } }
-
-        public CurrencyViewModel()
-        {
-
-        }
-
-        public void addCurrencies(dynamic CurrencyData)
-        {
-            foreach(dynamic CurrencyDataElement in CurrencyData.rates)
-            {
-                string currencyName = CurrencyDataElement.currency;
-                string currencyShortName = CurrencyDataElement.code;
-                string currencyPrice = CurrencyDataElement.mid;
-                string tempCurrencyCode = CurrencyDataElement.code;
-                string currencyCode = tempCurrencyCode.Remove(tempCurrencyCode.Length - 1);
-                this.currencies.Add(new Currency() { Name = currencyName, ShortName = currencyShortName, Price = currencyPrice, CountryImage = Currency.imagePathFirst + currencyCode.ToLower() + Currency.imagePathSecond});
-            }
-        }
-    }
+    
 
 }
---
+++ Projekt/CurrencyViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CurrencyNamespace;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyViewModelNamespace
+{
+    public class CurrencyViewModel
+    {
+
+        private ObservableCollection<Currency> currencies = new ObservableCollection<Currency>();
+        public ObservableCollection<Currency> Currencies { get { return this.currencies; } }
+
+        public CurrencyViewModel()
+        {
+
+        }
+
+        public void addCurrencies(dynamic CurrencyData)
+        {
+            foreach (dynamic CurrencyDataElement in CurrencyData.rates)
+            {
+                string currencyName = CurrencyDataElement.currency;
+                string currencyShortName = CurrencyDataElement.code;
+                string currencyPrice = CurrencyDataElement.mid;
+                string tempCurrencyCode = CurrencyDataElement.code;
+                string currencyCode = tempCurrencyCode.Remove(tempCurrencyCode.Length - 1);
+                this.currencies.Add(new Currency() { Name = currencyName, ShortName = currencyShortName, Price = currencyPrice, CountryImage = Currency.imagePathFirst + currencyCode.ToLower() + Currency.imagePathSecond });
+            }
+        }
+    }
+}
